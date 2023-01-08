d8cd97d33ac70aa8926d11a65795ac8b733e2a72
Gonajaime <jaime.gonzalez@bootcamp-upgrade.com>
2023-01-08 19:58:58 +0100

    second

--- main.js
+++ main.js
@@ -5,24 +5,23 @@ let newEmpty = document.querySelector(".empty");
 let newUl = document.querySelector("ul");
 
 newButton.onclick = function (ev) {
-    if (newInput$$.value == ""){
-        newEmpty.style.display = 'block';
+  if (newInput$$.value == "") {
+    newEmpty.style.display = "block";
 
-        return;
-        
-    } else{
-        newEmpty.style.display = 'none';         
-  let work = newInput$$.value;
-  let li = document.createElement("li");
-  li.textContent = work;
-  newUl.appendChild(li);
-
-  let btnDelete = document.createElement("button");
-  btnDelete.className = "btn-delete";
-  btnDelete.textContent = "X";
-  li.appendChild(btnDelete);
-  btnDelete.onclick = function () {
-    li.remove();
-  }};
+    return;
+  } else {
+    newEmpty.style.display = "none";
+    let work = newInput$$.value;
+    let li = document.createElement("li");
+    li.textContent = work;
+    newUl.appendChild(li);
+    let btnDelete = document.createElement("button");
+    btnDelete.className = "btn-delete";
+    btnDelete.textContent = "X";
+    li.appendChild(btnDelete);
+    btnDelete.onclick = function () {
+      li.remove();
+    };
+  }
   ev.preventDefault();
 };
